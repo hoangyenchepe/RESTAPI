1256663f5ccf639571d00b0009ff10702db95769
YenVoChePe <yen.vo@tekcent.com>
2022-05-24 17:41:58 +0700

    Implementing versioning API

--- Tweetbook/Tweetbook/Startup.cs
+++ Tweetbook/Tweetbook/Startup.cs
@@ -67,12 +67,7 @@ namespace Tweetbook
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
         }
     }
 }
