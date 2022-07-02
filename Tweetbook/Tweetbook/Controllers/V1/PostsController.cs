23c6e271810023d99e3e32759a3f385aa1289fb6
yendepgai <hoangyen090794@gmail.com>
2022-07-02 14:52:55 +0700

    Tutor5

--- Tweetbook/Tweetbook/Controllers/V1/PostsController.cs
+++ Tweetbook/Tweetbook/Controllers/V1/PostsController.cs
@@ -1,41 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Tweetbook.Contracts.V1;
 using Tweetbook.Contracts.V1.Requests;
 using Tweetbook.Contracts.V1.Responses;
 using Tweetbook.Domain;
+using Tweetbook.Services;
 
 namespace Tweetbook.Controllers.V1
 {
     public class PostsController : Controller
     {
-        private List<Post> _posts;
-        public PostsController()
+        private readonly IPostService _postService;
+        public PostsController(IPostService postService)
         {
-            _posts = new List<Post>();
-            for (int i = 0; i < 5; i++)
-            {
-                _posts.Add(new Post { Id = Guid.NewGuid().ToString() });
-            }
+            _postService = postService;
         }
 
         [HttpGet(ApiRoutes.Posts.GetAll)]
         public IActionResult GetAll()
         {
-            return Ok(_posts);
+            return Ok(_postService.GetPosts());
+        }
+
+        [HttpGet(ApiRoutes.Posts.Get)]
+        public IActionResult Get([FromRoute]Guid postId)
+        {
+            var post = _postService.GetPostById(postId);
+            if (post == null)
+                return NotFound();
+            return Ok(post);
         }
 
         [HttpPost(ApiRoutes.Posts.Create)]
         public IActionResult Create([FromBody] CreatePostRequest postRequest)
         {
             var post = new Post { Id = postRequest.Id };
-            if (string.IsNullOrEmpty(post.Id))
-                post.Id = Guid.NewGuid().ToString();
-            _posts.Add(post);
+            if (post.Id != Guid.Empty)
+                post.Id = Guid.NewGuid();
+            _postService.GetPosts().Add(post);
 
             var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
-            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id);
+            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id.ToString());
             var response = new PostResponse { Id = post.Id };
             return Created(locationUri, response);
         }
--- Tweetbook/Tweetbook/Installers/DbInstaller.cs
+++ Tweetbook/Tweetbook/Installers/DbInstaller.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Tweetbook.Data;
+using Tweetbook.Services;
 
 namespace Tweetbook.Installers
 {
@@ -15,6 +16,8 @@ namespace Tweetbook.Installers
                 configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<DataContext>();
+
+            services.AddSingleton<IPostService, PostService>();
         }
     }
 }
